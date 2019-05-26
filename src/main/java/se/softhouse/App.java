2e488e0e74eb383e919eeca8e9ee55ac02d7ce4b
Pezhman Kasraee <pezhman.kasraee@softhouse.se>
2019-05-26 22:44:04 +0200

    FIX: annotation added
       - Configuration annotations added
    
    Tracking-Id: 1905:00

--- src/main/java/se/softhouse/App.java
+++ src/main/java/se/softhouse/App.java
@@ -1,7 +1,10 @@
 package se.softhouse;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Author: Pezhman Kasraee
@@ -9,7 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App
 {
     public static void main( String[] args )
     {

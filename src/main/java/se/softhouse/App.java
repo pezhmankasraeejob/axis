237191567f7c87dd018e36ca89fdd837c48918e8
Pezhman Kasraee <pezhman.kasraee@softhouse.se>
2019-05-26 13:02:56 +0200

    ADD: main class added
    Tracking-Id: 1905:00

---
+++ src/main/java/se/softhouse/App.java
@@ -0,0 +1,18 @@
+package se.softhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Pezhman Kasraee
+ * Email: pezhman.kasraee@softhouse.se
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}

cce4d99edff61031b46b19f781c1340e0a53877d
Pezhman Kasraee <pezhman.kasraee@softhouse.se>
2019-05-27 23:53:26 +0200

    ADD: RequestParameter P added
    Tracking-Id: 1905:40

--- src/main/java/se/softhouse/controller/RecipeController.java
+++ src/main/java/se/softhouse/controller/RecipeController.java
@@ -1,15 +1,20 @@
 package se.softhouse.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import se.softhouse.domain.Recipe;
 import se.softhouse.service.RecipeService;
@@ -31,8 +36,21 @@ public class RecipeController
     @RequestMapping(value = "/recipe/{recipe_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     @Transactional
-    public Recipe getById(@PathVariable("recipe_id") Integer recipeId)
+    public Recipe getById(@PathVariable("recipe_id") Integer recipeId,
+            @RequestParam(value = "p", required = false) String pValue)
     {
+        System.out.println("P = " + pValue);
         return recipeService.getById(recipeId);
+    }
+
+    @RequestMapping(value = "/recipe", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @Transactional
+    public ResponseEntity<Object> addRecipe(@RequestBody Recipe recipe)
+    {
+        Recipe addedRecipe = recipeService.add(recipe);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(addedRecipe.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }

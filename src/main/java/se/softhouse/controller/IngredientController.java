186530043f6192ca905c7cda6ab6b0e5dd522569
Pezhman Kasraee <pezhman.kasraee@softhouse.se>
2019-05-28 02:30:40 +0200

    ADD: add method for Ingredient added
    Tracking-Id: 1905:40

--- src/main/java/se/softhouse/controller/IngredientController.java
+++ src/main/java/se/softhouse/controller/IngredientController.java
@@ -1,16 +1,20 @@
 package se.softhouse.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import se.softhouse.domain.Ingredient;
 import se.softhouse.service.IngredientService;
@@ -37,5 +41,16 @@ public class IngredientController
     {
         System.out.println("P = " + pValue);
         return ingredientService.getById(ingredientId);
+    }
+
+    @RequestMapping(value = "/ingredient", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @Transactional
+    public ResponseEntity<Object> addIngredient(@RequestBody Ingredient ingredient)
+    {
+        Ingredient addedIngredient = ingredientService.add(ingredient);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(addedIngredient.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }

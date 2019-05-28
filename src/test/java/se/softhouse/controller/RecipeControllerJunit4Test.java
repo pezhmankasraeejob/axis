bf05ea205e05b4047a5d25f6181dc2c2b4f13a21
Pezhman Kasraee <pezhman.kasraee@softhouse.se>
2019-05-28 13:45:05 +0200

    ADD: Spring Rest Doc creator
    Tracking-Id: 1905:70

---
+++ src/test/java/se/softhouse/controller/RecipeControllerJunit4Test.java
@@ -0,0 +1,48 @@
+package se.softhouse.controller;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeControllerJunit4Test
+{
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Rule
+    public JUnitRestDocumentation jUnitRestDocumentation = new JUnitRestDocumentation();
+
+    @Before
+    public void setup()
+    {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(this.jUnitRestDocumentation)).build();
+    }
+
+    @Test
+    public void getPersonByIdShouldReturnOk() throws Exception
+    {
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/recipe/{id}", 1)).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andDo(document("recipe/get-by-id"));
+    }
+}
